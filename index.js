538f3887b8cb836fe62d2b3a2fb90f02d1102cad
Kenzein <156581853+Kenzein@users.noreply.github.com>
2026-01-16 01:26:45 +0700

    Update and connect to frontend

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const userRoutes = require("./src/routes/userRoutes.js");
 const authRoutes = require("./src/routes/authRoutes.js");
 const buku = require("./src/routes/bukuRoutes.js");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/users", userRoutes);
